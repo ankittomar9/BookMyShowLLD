02c343398dd007f425465e4e82cbe580aad88b40
Ankit <tomarankit78@gmail.com>
2024-09-25 04:14:40 +0530

    BMS : Setup up Project and Code Models

---
+++ BookMyShowLLD/src/main/java/com/company/bookmyshowlld/BookMyShowLldApplication.java
@@ -0,0 +1,31 @@
+package com.company.bookmyshowlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowLldApplication.class, args);
+    }
+        /*   BaseModel baseModel=new BaseModel();
+           baseModel.getId();*/
+
+        //    BaseModel baseModel = new BaseModel(1,new Date(),new Date());
+
+}
+
+
+//Database : transform into the corresponding Object
+//ORM : Object relation mapping
+//Hibernate
+//Spring Data JPA : Like an interface
+
+
+//Phone pe: ICICIBank ,YesBank
+//BankAPI : dependency inversion
+
+//Hibernate has the implementation functions
+//Spring Data JPA : interface
+//Show findById
